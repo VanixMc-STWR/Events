708f0f540f9918e28bde78a3c7a4fbf1442c1047
Ali Alturfi <ali.alturfi@nordstrom.com>
2025-07-25 16:08:50 -0700

    Initial commit

---
+++ src/main/java/com/vanixmc/events/EventsPlugin.java
@@ -0,0 +1,17 @@
+package com.vanixmc.events;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EventsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
