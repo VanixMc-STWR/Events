8656b04605106030ae305da05685e89af6c36905
oschwa <114828394+oschwa@users.noreply.github.com>
2025-08-03 00:22:14 -0700

    Refactor worldguard integration to include RegionUtils class for helper functions. (#6)
    * Add worldguard dependency to maven
    
    * Refactor worldguard integration to include region-specific utility functions for future use
    
    * Add method overload for isLocationInRegion() for regionId lookup
    
    * refactor RegionUtils to include method overloads that utilize lambda expressions

---
+++ src/main/java/com/vanixmc/events/util/RegionUtils.java
@@ -0,0 +1,57 @@
+package com.vanixmc.events.util;
+
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class RegionUtils {
+
+    public static Optional<ProtectedRegion> getRegionById(String worldName, String regionId) {
+        return Optional.ofNullable(Bukkit.getWorld(worldName))
+                //  use BukkitAdapter to convert Bukkit world object to
+                //  WorldGuard object.
+                .map(BukkitAdapter::adapt)
+                //  Get RegionManager via instance of RegionContainer from
+                //  WorldGuard world.
+                .map(wgWorld -> WorldGuard.getInstance()
+                        .getPlatform().getRegionContainer().get(wgWorld))
+                //  return region via regionId.
+                .map(regionManager -> regionManager.getRegion(regionId));
+    }
+
+    public static Optional<ProtectedRegion> getRegionById(World world, String regionId) {
+        return Optional.ofNullable(world)
+                .map(BukkitAdapter::adapt)
+                .map(wgWorld -> WorldGuard.getInstance()
+                        .getPlatform().getRegionContainer().get(wgWorld))
+                //  return region via regionId.
+                .map(regionManager -> regionManager.getRegion(regionId));
+    }
+
+    public static boolean isLocationInRegion(Location location, String regionId) {
+        return getRegionById(Objects.requireNonNull(location.getWorld()),
+                regionId)
+                //  Given the ProtectedRegion determine if the
+                //  corresponding block vector to the location exists
+                //  in region.
+                .map(region ->
+                        region.contains(BukkitAdapter.asBlockVector(location)))
+                .orElse(false);
+    }
+
+    public static boolean isLocationInRegion(Location location, Region region) {
+        return Optional.ofNullable(location)
+                .map(BukkitAdapter::asBlockVector)
+                .filter(pos -> region != null)
+                .map(region::contains)
+                .orElse(false);
+    }
+}
