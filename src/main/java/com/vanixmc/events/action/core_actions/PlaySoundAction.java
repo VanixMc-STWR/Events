cb49f17e6685f2b5ff1a7fe6043e7f6d48c690a2
Ali <ogaliwork@gmail.com>
2025-09-01 20:15:43 -0700

    Feature: Simplify global sound playback logic in PlaySoundAction

--- src/main/java/com/vanixmc/events/action/core_actions/PlaySoundAction.java
+++ src/main/java/com/vanixmc/events/action/core_actions/PlaySoundAction.java
@@ -2,16 +2,12 @@ package com.vanixmc.events.action.core_actions;
 
 import com.vanixmc.events.action.domain.AbstractAction;
 import com.vanixmc.events.context.Context;
-import com.vanixmc.events.event.zone_event.ZoneEvent;
 import com.vanixmc.events.shared.ConfigBuilder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
-import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-
-import java.util.Objects;
 
 @Getter
 @ToString
@@ -25,22 +21,9 @@ public class PlaySoundAction extends AbstractAction {
     @Override
     public boolean execute(Context context) {
         if (global) {
-            if (context.getEvent() != null) {
-                if (context.getEvent() instanceof ZoneEvent zoneEvent) {
-                    zoneEvent.getPlayersInZone()
-                            .stream()
-                            .map(Bukkit::getPlayer)
-                            .filter(Objects::nonNull)
-                            .forEach(p -> p.playSound(p.getLocation(), sound, volume, pitch));
-                    return true;
-                }
-            } else {
-                Bukkit.getOnlinePlayers().forEach(p ->
-                        p.playSound(p.getLocation(), sound, volume, pitch)
-                );
-                return true;
-            }
-            return false;
+            getGlobalScopePlayers()
+                    .forEach(p -> p.playSound(p.getLocation(), sound, volume, pitch));
+            return true;
         }
         Player player = context.getPlayer();
         if (player == null) return false;
